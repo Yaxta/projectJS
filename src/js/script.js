264875c4d092310ca0c20deda183dff9efb45f59
Yaxta <example@gmail.com>
2023-05-15 12:52:33 +0300

    first commit

---
+++ src/js/script.js
@@ -0,0 +1,10 @@
+'use sctrict';
+
+
+const ansers = [];
+
+ansers[0] = prompt('Как Ваше имя?', '');
+ansers[1] = prompt('Как Ваше Фам?', '');
+ansers[2] = prompt('Как Ваше ничего?', '');
+
+document.write(ansers);
